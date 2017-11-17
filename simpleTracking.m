5ece311024760af72d85a2f6d04718306d992c8c
Andre Almeida <afernandesalmeida@outlook.com>
2017-11-17 19:29:44 -0200

    acceptable tracking

--- simpleTracking.m
+++ simpleTracking.m
@@ -9,3 +9,28 @@ end
 if size(size(im2),2)==3
     im2=im2double(rgb2gray(im2));
 end
+
+%% Image difference
+
+ret = imabsdiff(im2,im1);
+
+%% Find corners
+C = corner(ret,'Harris',2); %Magic happens here
+
+x_mean = mean(C(:,1));
+y_mean = mean(C(:,2));
+
+position = [x_mean y_mean];
+
+%% Tracking and centroid plot
+h=figure;
+imshow(ret);
+hold on;
+plot(x_mean,y_mean,'r*');
+hold on;
+plot(C(:,1), C(:,2), 'b*');
+
+% Plot save
+saveas(h,sprintf('../../dataOutput/fullReal/tracking/fullHorizontal/fullHorizontal%d.png',i));
+
+end
